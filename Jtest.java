757600e728da732c6a22f8c6d97c611ed43664ad
JJ <jying@hawk.iit.edu>
2015-01-25 02:07:51 -0600

    nnn

--- Jtest.java
+++
@@ -1,9 +0,0 @@
-public class Jtest {
-    public static void main(String [] args) {
-        Cashier c = new Cashier();
-  		c.addItem("123");
-  		c.addItem("234");
-  		c.sum();
-  		c.tax();
-    }
-}
