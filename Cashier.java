b64f756fe11d75f075343cc246097c570bb95534
Shuyu Zhang <merge.tool>
2015-01-25 00:32:12 -0600

    fixed the default construct

--- Cashier.java
+++ Cashier.java
@@ -1,1 +1,37 @@
+import Item;
+import Scanning;
+import java.util.*;
+
 public class Cashier
+{
+	Scanning sc = new Scanning();
+	ArrayList<Item> = new	
+
+	public void addItem(String barcode)
+	{
+		sc.loadInfomation();
+		i = sc.scan(barcode);
+	}
+
+	public void deleteItem(String barcode)
+	{
+		sc.loadInfomation();
+
+	}
+
+	public double sum()
+	{
+
+	}
+
+	public double tax()
+	{
+
+	}
+
+	public String print()
+	{
+
+	}
+
+}
