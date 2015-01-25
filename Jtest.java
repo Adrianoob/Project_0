9aa101e44629330b10f78b550c388ad9de2a2dc2
JJ <jying@hawk.iit.edu>
2015-01-25 00:50:15 -0600

    list updated

--- Jtest.java
+++
@@ -1,9 +0,0 @@
-public class Jtest {
-    public static void main(String [] args) {
-        Scanning sc = new Scanning();
-        sc.loadInformation();
-        Item item = sc.scan("123");
-        System.out.println(item.toString());
-        //System.out.println(sc.toString());
-    }
-}
