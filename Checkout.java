ff3faa413a718f5c926c67971fc99c398091e29f
JJ <jying@hawk.iit.edu>
2015-01-25 02:51:54 -0600

    Checkout.java

--- Checkout.java
+++ Checkout.java
@@ -2,7 +2,20 @@ import java.util.Scanner;
 public class Checkout {
     public static void main(String[]args) {
         Cashier cashier = new Cashier();
-        System.out.println("You can start to scan your first item.");
+        System.out.println("    You can start to scan your first item.");
+        System.out.print("    First one>");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
         
+        while(!input.equals("pay")) {
+            cashier.addItem(input);
+            System.out.print("    Next item>");
+            input = scan.next();
+        }
+        
+        double sum = cashier.sum();
+        double tax = cashier.tax();
+        
+        System.out.println(cashier.print());
     }
 }
