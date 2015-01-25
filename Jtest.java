c3dd4f73104fded16bde8b6e17e776eec6d5c7a6
JJ <jying@hawk.iit.edu>
2015-01-25 00:24:24 -0600

    nnn

---
+++ Jtest.java
@@ -0,0 +1,7 @@
+public class Jtest {
+    public static void main(String [] args) {
+        Scanning sc = new Scanning();
+        sc.loadInformation();
+        sc.toString();
+    }
+}
--- Scanning.java
+++ Scanning.java
@@ -1,16 +1,51 @@
-import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Scanning{
-    ArrayList item = null;
+    Item [] item = null;
     public Scanning() {
-        item = new ArrayList();
+        item = null;
     }
     
     public void loadInformation() {
-        //
+        try {
+            File f0 = new File("list.txt");
+            Scanner scan0 = new Scanner(f0);
+            int c = 0;
+            while (scan0.hasNextLine()) {
+                c++;
+                String temp = scan0.nextLine();
+            }
+            
+            item = new Item[c];
+            
+            File f = new File("list.txt");
+            Scanner scan = new Scanner(f);
+            for (int i = 0; i < c; i++) {
+                String temp = scan.nextLine();
+                StringTokenizer tok = new StringTokenizer(temp,",");
+                String barcode = tok.nextToken();
+                String name = tok.nextToken();
+                double price = Double.parseDouble(tok.nextToken());
+                item[i] = new Item(barcode, name, price);
+            }
+        } catch (IOException ioe) {
+            System.out.println("Error reading file");
+        }
     }
     
     public String scan(String barcode) {
+        
         //the return type need to be changed to be Item.
         return "something";
+    }
+    
+    public String toString() {
+        String res = "";
+        for (int i = 0; i < item.length; i++) {
+            res = res + item[i];
+        }
     }
 }
